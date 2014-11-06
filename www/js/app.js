fc54613379eb4805ede576dfd79cadf4bd9a94d1
cameronbourke <cameron2295@gmail.com>
2014-11-06 16:49:44 +1100

    Careful Commit
    Commiting before I attempt to refactor the js.
    
    Signed-off-by: cameronbourke <cameron2295@gmail.com>

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,35 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('ionicApp', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+  .controller('AppCtrl', function($scope, $ionicModal) {
+
+    $scope.contacts = [{ 
+      title: 'Car Money',
+      fullName: 'Gordon Freeman',
+      dollarAmount: '15',
+      description: 'Dropped Gordo home from town.'
+    },
+    { 
+      title: 'Drug Money',
+      fullName: 'Isaac Shoeman',
+      dollarAmount: '30',
+      description: 'So many drugs in his pocket that I need.'
+    }];
+
+    $ionicModal.fromTemplateUrl('templates/addTheyo.html', {
+      scope: $scope
+      }).then(function(modal) {
+      $scope.modal = modal;
+    });
+
+    $scope.createContact = function(u) {        
+      $scope.contacts.push({ fullName: u.fullName },
+                           { title: u.title },
+                           { dollarAmount: u.dollarAmount },
+                           { description: u.description });
+      $scope.modal.hide();
+    };
+
   });
-})
