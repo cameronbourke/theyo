5fb1367ec05ceee851df0134dc095c2c56219763
cameronbourke <cameron2295@gmail.com>
2014-11-09 17:28:57 +1100

    LocalStorage Working
    Made the changes to app.js to use localstorage.
    
    Signed-off-by: cameronbourke <cameron2295@gmail.com>

--- www/js/app.js
+++ www/js/app.js
@@ -15,18 +15,23 @@ angular.module('ionicApp', ['ionic'])
     });
   })
 
-  .controller('AppCtrl', function($scope, $ionicModal) {
+.controller('AppCtrl', function($scope, $ionicModal) {
 
-    window.localStorage['items'] = JSON.stringify([{ 
-      title: 'You Owe Me Now',
-      fullName: 'Cameron Bourke',
-      dollarAmount: '2',
-      description: 'You owe me $2 for downloading this app. Only kidding!! This is just an example of a "Theyo". Delete me now by swiping to the left and enjoy.'
-    }]);
+    // check if localStorage['items'] is present
+    if (!window.localStorage.items) {
+
+      window.localStorage['items'] = JSON.stringify([{
+        title: 'You Owe Me Now',
+        fullName: 'Cameron Bourke',
+        dollarAmount: '2',
+        description: 'You owe me $2 for downloading this app. Only kidding!! This is just an example of a "Theyo". Delete me now by swiping to the left and enjoy.'
+      }]);
+
+    }
 
     $scope.contacts = JSON.parse(localStorage['items']) || [];
 
-    $scope.createContact = function(u) {     
+    $scope.createContact = function(u) {
       $scope.contacts.push({ fullName: u.fullName,
                              title: u.title,
                              dollarAmount: u.dollarAmount,
@@ -35,11 +40,14 @@ angular.module('ionicApp', ['ionic'])
       u.title = '';
       u.dollarAmount = '';
       u.description = '';
+
+      window.localStorage.items = JSON.stringify($scope.contacts);
       $scope.modal.hide();
     };
 
     $scope.onItemDelete = function(item) {
       $scope.contacts.splice($scope.contacts.indexOf(item), 1);
+      window.localStorage.items = JSON.stringify($scope.contacts);
     };
 
     $ionicModal.fromTemplateUrl('templates/addTheyo.html', {
@@ -48,4 +56,4 @@ angular.module('ionicApp', ['ionic'])
      $scope.modal = modal;
     });
 
-  });      
+  });
